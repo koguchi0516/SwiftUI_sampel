e0e536e2b52d0d409d36a2e9e5d93a89c8ee60d8
koguchi0516 <koguchi.shohei@gmail.com>
2023-09-26 14:50:24 +0900

    Initial Commit

---
+++ practice_app/practice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_appApp.swift
+//  practice_app
+//
+//  Created by Koguchi on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct practice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
